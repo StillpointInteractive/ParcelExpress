2911a77301b0ae09053e973df565cc531e98240d
oXhnieX111 <stillpointinteractive@gmail.com>
2026-06-29 16:05:40 +0100

    Added jumping to the basic player movement
    The basic movement system is now complete. The player can move in 8 direction, look around and jump.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,14 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 moveInput;
     private Vector2 lookInput;
+
     private float xRotation = 0f;
+    private float verticalVelocity;
 
     [SerializeField] private float walkSpeed = 5f;
-    [SerializeField] private float mouseSensitivity = .1f;
+    [SerializeField] private float mouseSensitivity = 0.1f;
+    [SerializeField] private float jumpForce = 4f;
+
     [SerializeField] private Transform cameraTransform;
     
     
@@ -41,19 +45,37 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = playerControls.Player.Move.ReadValue<Vector2>();
         lookInput = playerControls.Player.Look.ReadValue<Vector2>();
+        
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+
+        if (controller.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2;
+        }
+
+        if(playerControls.Player.Jump.WasPressedThisFrame() && controller.isGrounded)
+        {
+            verticalVelocity = jumpForce;
+        }
+
 
         Vector3 moveDirection = transform.right * moveInput.x
             + transform.forward * moveInput.y;
+        moveDirection.y = verticalVelocity;
 
         transform.Rotate(Vector3.up * lookInput.x * mouseSensitivity);
 
         xRotation -= lookInput.y * mouseSensitivity;
-        xRotation = Mathf.Clamp(xRotation, -55f, 55f);
+        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
         cameraTransform.localRotation = Quaternion.Euler(xRotation, 0, 0);
        
         controller.Move(moveDirection * walkSpeed * Time.deltaTime);
 
-        Debug.Log(xRotation);
+       
+
+        
     }
 
 }
